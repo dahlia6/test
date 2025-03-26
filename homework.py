a7150c69df903e8f919d559168b7e89cb0010ff9
dahlia6 <dahliazhou007@gmail.com>
2025-03-26 17:22:28 +1300

    Create homework.py

---
+++ homework.py
@@ -0,0 +1,12 @@
+minutes=int(input("minutes covert to seconds"))
+seconds=60*minutes
+print(seconds,"seconds")
+
+cards=int(input("how many cards Jane gets as a minimum"))
+years=2*cards
+print("Jane receives at least",years,"cards")
+
+days=int(input("years convert to days"))
+years=365*days
+print("You are",years,"days old")
+
